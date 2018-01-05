9e6c812c62cd2ada3f50f21fdbe89b08853272e0
Derek Thurn <derek@fake.email>
2018-01-04 22:39:35 -0800

    initial commit

---
+++ Assets/Source/LookAtProbe.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtProbe : MonoBehaviour {
+
+    public Transform FollowTarget;
+    private RtsCamera _rtsCamera;
+    private Vector3 _offset;
+
+	private void Start () {
+        _rtsCamera = GetComponent<RtsCamera>();
+        _rtsCamera.Follow(FollowTarget, true);
+    }
+}
