dc1fe7714d8f2a9045a414ba5245847be74b6e05
Derek Thurn <derek@fake.email>
2018-01-06 11:42:19 -0800

    selection of stuff

---
+++ Assets/Source/SelectionManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectionManager : MonoBehaviour {
+
+    public UIWindow ControlWindow;
+    private GameObject _currentlySelected;
+
+	void Update ()
+    {
+		if (Input.GetMouseButtonDown(0))
+        {
+            if (_currentlySelected)
+            {
+                Destroy(_currentlySelected.GetComponent<cakeslice.Outline>());
+            }
+            _currentlySelected = null;
+
+            RaycastHit hit;
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                var objectHit = hit.transform.gameObject;
+                if (objectHit.tag.Equals("Selectable"))
+                {
+                    var outline = objectHit.AddComponent<cakeslice.Outline>();
+                    outline.color = 1;
+                    _currentlySelected = objectHit;
+                }
+            }
+
+            if (_currentlySelected)
+            {
+                ControlWindow.Show();
+            }
+            else
+            {
+                ControlWindow.Hide();
+            }
+        }
+	}
+}
